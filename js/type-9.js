cd2a163534fec5db8141e0684015f3e2a19a5e29
SerhiiSamara <ndaair@gmail.com>
2022-09-21 21:58:03 +0300

    Виправив. Повинен робити сайт.

--- js/type-9.js
+++ js/type-9.js
@@ -1,6 +1,7 @@
 const body = document.body;
 const span = document.querySelector(".color");
-const button = document.addEventListener("click", changeColor);
+const button = document.querySelector(".change-color");
+button.addEventListener("click", changeColor);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
